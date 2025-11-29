e90b3336b5e6c5635ab1a9fb00dbc834d0264fb1
Flo <f.carriere@net.estia.fr>
2025-11-29 23:42:09 +0100

    add DB

---
+++ Correlation_Back/Correlation/src/test/java/com/Flo/Correlation/CorrelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Flo.Correlation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrelationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
